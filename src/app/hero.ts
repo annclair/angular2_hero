2ee355ebcc4c5e8de80ba5a1122d45784adbb951
annclair <anneclaire.sebirou@gmail.com>
2017-09-12 12:08:43 +0200

    mock heroes data & heroes service

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,5 +1,10 @@
+// export class Hero {
+//     constructor(
+//       public id: number,
+//       public name: string) { }
+//   }
+
 export class Hero {
-    constructor(
-      public id: number,
-      public name: string) { }
-  }
+  id: number;
+  name: string;
+}
