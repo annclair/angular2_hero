972094028ef88f096162c9b74368cbcd47feffe5
annclair <anneclaire.sebirou@gmail.com>
2017-09-12 19:43:17 +0200

    update hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,27 +7,40 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+    
+    private headers = new Headers({'Content-Type': 'application/json'});
     private heroesUrl = 'api/heroes';  // URL to web api
     
     constructor(private http: Http) { }
         
     getHeroes(): Promise<Hero[]> {
-        return this.http.get(this.heroesUrl)
+        return this.http
+            .get(this.heroesUrl)
             .toPromise()
             .then(response => response.json().data as Hero[])
             .catch(this.handleError);
     }
         
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
-    }
-
     getHero(id: number): Promise<Hero> {
         const url = `${this.heroesUrl}/${id}`;
-        return this.http.get(url)
+        return this.http
+            .get(url)
             .toPromise()
             .then(response => response.json().data as Hero)
             .catch(this.handleError);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,10 +1,10 @@
-// export class Hero {
-//     constructor(
-//       public id: number,
-//       public name: string) { }
-// }
-
 export class Hero {
-  id: number;
-  name: string;
+    constructor(
+      public id: number,
+      public name: string) { }
 }
+
+// export class Hero {
+//   id: number;
+//   name: string;
+// }
