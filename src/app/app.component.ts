4648f4adaf4d05d29c677f26a816e24f584fa3cb
annclair <anneclaire.sebirou@gmail.com>
2017-09-12 10:56:11 +0200

    add a first hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+//import Hero class create in hero.ts
+import { Hero } from './hero';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero = new Hero(1, 'Windstorm');
+ 
 }
